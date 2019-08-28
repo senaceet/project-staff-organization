57a16eb8eaafbe14f6b066a04056690ff202ef0d
27blessed19 <smunoz8501@misena.edu.cp>
2019-08-28 11:53:07 -0500

    docs:  Se realiza cambio de archivos en la carpeta de SQL Server

--- 3-Desarrollo/02. Base datos SQL Server/Create_table_staff_organizacion .sql
+++ 3-Desarrollo/02. Base datos SQL Server/Create_table_staff_organizacion .sql
@@ -2,15 +2,15 @@ create database staff_organization;
 use staff_organization;
 
 create table tipo_documento(
-id_tipo_documento int  not null,
-abreviacion varchar (10) not null,
-nombre_documento varchar (20) not null,
+id_tipo_documento smallint not null,
+abreviacion varchar (5) not null,
+nombre_documento varchar (30) not null,
 primary key (id_tipo_documento)
 );
 
 create table rol(
-id_rol int not null,
-nombre_rol varchar (20),
+id_rol int  not null,
+nombre_rol varchar (30),
 primary key (id_rol)
 );
 
@@ -24,11 +24,10 @@ primary key (id_tipo_donacion)
 create table donacion(
 id_donacion int not null,
 valor_donacion int not null,
-fecha_donacion date not null,
-hora_donacion time not null,
+fecha_donacion datetime not null,
 id_tipo_donacion int not null,
-id_numero_documento int not null,
-id_tipo_documento int not null
+id_numero_documento bigint not null,
+id_tipo_documento smallint not null
 );
 
 create table sede(
@@ -36,7 +35,7 @@ id_sede int not null,
 nombre_sede varchar (45) not null,
 direccion varchar (25) not null,
 propietario varchar (10) not null,
-telefono bigint (20) not null,
+telefono bigint  not null,
 primary key (id_sede)
 );
 
@@ -59,25 +58,23 @@ primary key (id_reunion)
 
 create table horarios(
 id_horarios int not null,
-duracion time not null,
-hora time not null,
-fecha date not null,
+fecha datetime not null,
 id_reunion int not null,
 primary key (id_horarios)
 );
 
 create table usuario(
-id_numero_documento int not null,
+id_numero_documento bigint not null,
 primer_nombre varchar(25) not null,
 segundo_nombre  varchar(25) not null,
 primer_apellido varchar (25) not null,
 segundo_apellido varchar(25) not null,
-telefono bigint (20) not null,
-email varchar(25) not null unique,
-contraseÃ±a varchar(30) not null,
-fecha_nacimiento date not null,
+telefono bigint  not null,
+email varchar(50) not null unique,
+contraseña varchar(50) not null,
+fecha_nacimiento datetime not null,
 estado_civil varchar(25) not null,
-id_tipo_documento int not null,
+id_tipo_documento smallint not null,
 id_sede int not null,
 id_rol int not null
 );
@@ -98,40 +95,24 @@ primary key(id_servidor)
 
 -- Usuario/tipo documento --
 alter table usuario add constraint fk_usuario_tipo_documento foreign key (id_tipo_documento) references tipo_documento(id_tipo_documento) on update cascade;
-alter table usuario add index fk_usuario_tipo_documento_idx (id_tipo_documento);
 alter table usuario add constraint fk_rol_usuario foreign key (id_rol) references rol(id_rol) on update cascade;
-alter table usuario add index fk_rol_usuario_idx (id_rol);
 alter table usuario add constraint fk_sede_usuario foreign key (id_sede) references sede(id_sede) on update cascade;
-alter table usuario add index fk_sede_usuario_idx (id_sede);
 alter table usuario add primary key (id_tipo_documento, id_numero_documento);
 
 -- Usuario/donacion--
 alter table donacion add constraint fk_donacion_usuario foreign key (id_tipo_documento, id_numero_documento) references usuario(id_tipo_documento, id_numero_documento) on update cascade;
-alter table donacion add index fk_donacion_usuario_idx (id_tipo_documento, id_numero_documento);
 alter table donacion add constraint fk_donacion_tipo_donacion foreign key (id_tipo_donacion) references tipo_donacion(id_tipo_donacion) on update cascade;
-alter table donacion add index fk_donacion_tipo_donacion_idx(id_tipo_donacion);
 alter table donacion add primary key (id_tipo_donacion, id_donacion);
 
 -- Usuario/reunion --
 alter table reunion add constraint fk_sede_reunion foreign key (id_sede) references sede(id_sede) on update cascade;
-alter table reunion add index fk_sede_reunion_idx (id_sede);
+
 
 -- Usuario/horario --
 alter table horarios add constraint fk_horario_reunion foreign key (id_reunion) references reunion(id_reunion) on update cascade;
-alter table horarios add index fk_horario_reunion_idx (id_reunion);
+
 
 -- Usuario/mantenimiento_sede --
 alter table mantenimiento_sede add constraint fk_mantenimiento_sede_sede foreign key (id_sede) references sede(id_sede) on update cascade;
-alter table mantenimiento_sede add index fk_mantenimiento_sede_sede_idx (id_sede);
-
-
-
-
-
-
-
-
-
-
 
 
